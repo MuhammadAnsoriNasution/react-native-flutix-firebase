41baa15aa5d9876d4431f3143ef68b690c0a1d57
muhammad ansori nasution <muhammadansorinasution@gmail.com>
2023-10-10 11:55:01 +0700

    thmees color

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,11 +1,57 @@
+import { StyleSheet } from 'react-native';
+import fontFamily from '../assets/fonts';
+
+const defaultMargin = 24;
 const mainColor = '#503E9D';
 const accentColor1 = '#2C1F63';
 const accentColor2 = '#FBD460';
-const accentColor3 = '#ADADAD';
+const greenColor = '#3E9D9D';
+const redColor = '#FF5C83';
+const blackColor = '#000';
+const greyColor = '#E4E4E4';
+const greyColor2 = '#BEBEBE';
+const greyColor3 = '#ADADAD';
+const secondary = '#F6F6F6';
+const secondary2 = '#DDDCDC';
+
+const styles = StyleSheet.create({
+  blackTextFont: {
+    fontFamily: fontFamily.raleWay[500],
+    color: blackColor,
+  },
+  whiteTextFont: {
+    color: 'white',
+    fontFamily: fontFamily.raleWay[500],
+  },
+  purpleTextFont: {
+    fontFamily: fontFamily.raleWay[500],
+    color: mainColor,
+  },
+  greyTextFont: {
+    fontFamily: fontFamily.raleWay[500],
+    color: greyColor3,
+  },
+  whiteNumberFont: {
+    fontFamily: fontFamily.openSans[400],
+    color: 'white',
+  },
+  yellowNumberFont: {
+    fontFamily: fontFamily.openSans[400],
+    color: accentColor2,
+  },
+});
 
 export default {
   mainColor,
   accentColor1,
   accentColor2,
-  accentColor3,
+  greyColor3,
+  defaultMargin,
+  greenColor,
+  greyColor2,
+  secondary,
+  greyColor,
+  redColor,
+  secondary2,
+  styles,
 };
