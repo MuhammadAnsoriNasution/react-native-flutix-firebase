48067b29f84cb51312438b569e4a8782662dfabe
muhammad ansori nasution <muhammadansorinasution@gmail.com>
2023-10-11 09:43:50 +0700

    add screen

---
+++ src/screens/TicketScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function TicketScreen() {
+  return (
+    <View>
+      <Text>TicketScreen</Text>
+    </View>
+  );
+}
---
+++ src/screens/TopUpScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function TopUpScreen() {
+  return (
+    <View>
+      <Text>TopUpScreen</Text>
+    </View>
+  );
+}
