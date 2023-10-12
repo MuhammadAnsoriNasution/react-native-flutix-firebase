2ed51fd0f36c28dfe7e73d7757eb54f5c28822a5
muhammad ansori nasution <muhammadansorinasution@gmail.com>
2023-10-12 10:01:22 +0700

    textinput

--- src/components/atoms/input/TextInput.tsx
+++ src/components/atoms/input/TextInput.tsx
@@ -1,20 +1,53 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, { useRef, useState } from 'react';
 import {
-  View,
-  Text,
-  TextInput as TextInputR,
   StyleSheet,
+  Text,
   TextInputProps,
+  TextInput as TextInputR,
+  View,
 } from 'react-native';
-import React from 'react';
+import theme from '../../../utils/theme';
 
 interface Props extends TextInputProps {
   label: string;
 }
-export default function TextInput({ label, ...props }: Props) {
+export default function TextInput({ label, style, ...props }: Props) {
+  const [isFocus, setIsFocus] = useState(false);
+  const refInput = useRef<TextInputR>(null);
   return (
-    <View style={[styles.container]}>
-      <Text>{label}</Text>
-      <TextInputR {...props} />
+    <View style={[styles.container, style]}>
+      <Text
+        onPress={() => {
+          setIsFocus(true);
+          refInput.current?.focus();
+        }}
+        style={[
+          styles.label,
+          {
+            top: isFocus ? -9 : 15,
+            color: isFocus ? theme.accentColor2 : theme.greyColor2,
+          },
+        ]}>
+        {label}
+      </Text>
+      <View
+        style={[
+          styles.wrapperInput,
+          { borderColor: isFocus ? theme.accentColor2 : theme.greyColor2 },
+        ]}>
+        <TextInputR
+          ref={refInput}
+          style={[styles.textinput]}
+          {...props}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => {
+            if (props.value === '' || props.value === undefined) {
+              setIsFocus(false);
+            }
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -22,5 +55,26 @@ export default function TextInput({ label, ...props }: Props) {
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
+  },
+  wrapperInput: {
+    position: 'relative',
+    borderWidth: 1,
+    borderColor: theme.greyColor2,
+    padding: 13,
+    borderRadius: 6,
+    zIndex: 0,
+  },
+  label: {
+    ...theme.styles.greyTextFont,
+    position: 'absolute',
+    left: 20,
+    zIndex: 10,
+    color: theme.greyColor2,
+    backgroundColor: theme.whiteColor,
+    paddingHorizontal: 5,
+  },
+  textinput: {
+    ...theme.styles.blackTextFont,
+    fontSize: 16,
   },
 });
--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -1,15 +1,46 @@
+/* eslint-disable react-native/no-inline-styles */
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RootStackParamList } from '../routes';
-
+import theme from '../utils/theme';
+import * as images from './../assets/images';
+import TextInput from '../components/atoms/input/TextInput';
 type Props = NativeStackScreenProps<RootStackParamList, 'SignInScreen'>;
 
 export default function SignInScreen({}: Props) {
   return (
-    <SafeAreaView>
-      <Text>SignInScreen</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Image source={images.logo} style={styles.logo} />
+        <Text style={styles.title}>{'Welcome Back,\nExplorer!'}</Text>
+        <TextInput label="Email Address" style={{ marginBottom: 27 }} />
+        <TextInput label="Email Address" style={{ marginBottom: 27 }} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    paddingHorizontal: theme.defaultMargin,
+    paddingTop: 30,
+    flex: 1,
+    backgroundColor: theme.whiteColor,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  logo: {
+    height: 70,
+    width: 67,
+    marginBottom: 70,
+  },
+  title: {
+    ...theme.styles.blackTextFont,
+    fontSize: 20,
+    marginBottom: 51,
+  },
+});
